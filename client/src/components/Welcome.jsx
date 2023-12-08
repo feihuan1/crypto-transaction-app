544fc01563f9da40a1194d98b2c22171c54ca693
eric <you@example.comericrealtor1997@gmail.com>
2023-12-08 11:05:18 -0800

    welcome page

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import { AiFillPlayCircle } from "react-icons/ai";
+import { SiEthereum } from "react-icons/si";
+import { BsInfoCircle } from "react-icons/bs";
+
+import Loader from "./Loader";
 
 const Welcome = () => {
   return (
-    <div>
-      Welcome
+    <div className="flex w-full justify-center items-center">
+        <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+          <div className="flex flex-1 justify-start flex-col md:mr-10">
+              <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
+                Send Crypto <br /> across the world
+              </h1>
+          </div>
+        </div>
     </div>
-  )
-}
+  );
+};
 
-export default Welcome
+export default Welcome;
