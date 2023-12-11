c546114a3e07f0e85bfe0e800f3dc7dc071fa760
eric <you@example.comericrealtor1997@gmail.com>
2023-12-11 10:57:43 -0800

    stucked on run script

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,5 +1,13 @@
+// https://polygon-mainnet.g.alchemy.com/v2/MQwc3jxnI-i2I33EYyl0VAxryFKWYvi5
+
 require('@nomiclabs/hardhat-waffle');
 
 module.exports = {
   solidity: '0.8.0',
+  networks: {
+    polygon: {
+      url: 'https://polygon-mainnet.g.alchemy.com/v2/MQwc3jxnI-i2I33EYyl0VAxryFKWYvi5', // Update with your Alchemy API key
+      accounts: ['2837e4b49e6bce7e83638547795de52a961f245cc217b179c1e35dc063ca7f4c'], // Add your testnet account private key
+    },
+  },
 };
